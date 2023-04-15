db739ef00787c4ffb64ff0558b6a95083720c34c
Filip Velebit Samodol <filip.samodol@coreline.agency>
2023-04-16 00:31:29 +0200

    Navbar changes

--- src/pages/LandingPage/NavBar.tsx
+++ src/pages/LandingPage/NavBar.tsx
@@ -42,22 +42,15 @@ export const NavBar = () => {
                 {name}
               </Nav.Link>
             ))}
-            <Nav.Link css={navbarLinksStyle} onClick={() => handleLogout()}>
+          </Nav>
+          <Nav>
+            <Nav.Link
+              css={navbarLinksStyle}
+              className="justify-content-end"
+              onClick={() => handleLogout()}
+            >
               Logout
             </Nav.Link>
-            {/* <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
